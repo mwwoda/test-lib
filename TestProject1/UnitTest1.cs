3ffeefa12c471920e1378e1053e179c149cebf7b
mwoda <84077698+mwwoda@users.noreply.github.com>
2025-09-10 14:13:23 +0200

    chore: add cron workflow

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestProject1Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ TestProjectCron/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TestProjectCron
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestProjectCronTest1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
